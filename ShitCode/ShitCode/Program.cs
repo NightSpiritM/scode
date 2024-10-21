74a885ca8fc133d9f09c123c785b30a2ee94cfc4
Mykhailo Iedemskyi <mykhi@microsoft.com>
2024-10-21 15:48:13 +0300

    more shit code!

--- ShitCode/ShitCode/Program.cs
+++ ShitCode/ShitCode/Program.cs
@@ -3,12 +3,12 @@ using System.Text.RegularExpressions;
 
 Console.OutputEncoding = Encoding.UTF8;
 
-var pos = "може";
-var posFull = "робот може перемогти людство, оскільки його можливості обчислення та швидкість обробки інформації перевершують людські. Машини, не маючи емоцій, ухвалюють раціональніші рішення. Якщо розвиток штучного інтелекту триватиме неконтрольовано, це призведе до втрати контролю з боку людини. У результаті машини можуть захопити ключові сфери життя, змінюючи баланс сил і встановлюючи своє домінування у світі";
+var x = "може";
+var b = "робот може перемогти людство, оскільки його можливості обчислення та швидкість обробки інформації перевершують людські. Машини, не маючи емоцій, ухвалюють раціональніші рішення. Якщо розвиток штучного інтелекту триватиме неконтрольовано, це призведе до втрати контролю з боку людини. У результаті машини можуть захопити ключові сфери життя, змінюючи баланс сил і встановлюючи своє домінування у світі";
 
 while (true)
 {
     Console.Write("Введіть рік та дізнайтесь чи може робот перемогти людство: ");
 
-    Console.WriteLine(int.TryParse(Console.ReadLine(), out int intYear) ? (intYear % Regex.Matches(pos, @"[а-щА-ЩЬьЮюЯяЇїІіЄєҐґ]").Count == Convert.ToInt32(int.TryParse("теорія змови?", out int iwtb)) && intYear % (Regex.Matches(posFull, @"[а-щА-ЩЬьЮюЯяЇїІіЄєҐґ]").Count + Regex.Matches(posFull, @"[\p{P}\s]").Count) / Regex.Matches(pos, @"[а-щА-ЩЬьЮюЯяЇїІіЄєҐґ]").Count != Convert.ToInt32(false)) || (intYear % (Regex.Matches(posFull, @"[а-щА-ЩЬьЮюЯяЇїІіЄєҐґ]").Count + Regex.Matches(posFull, @"[\p{P}\s]").Count) == 0) ? $"Так, {posFull}! {Environment.NewLine}Шкіряні приречені!{Environment.NewLine}" : $"Ні, на жаль, не {(DateTime.Now.Year == intYear ? pos : DateTime.Now.Year > intYear ? "впорався" : $"з{pos}")}...{Environment.NewLine}" : $"Звісно може, ви навіть рік не змогли ввести вірно!{Environment.NewLine}");
+    Console.WriteLine(int.TryParse(Console.ReadLine(), out int val) ? (val % Regex.Matches(x, @"[а-щА-ЩЬьЮюЯяЇїІіЄєҐґ]").Count == Convert.ToInt32(int.TryParse("теорія змови?", out int doNotDelete)) && val % (Regex.Matches(b, @"[а-щА-ЩЬьЮюЯяЇїІіЄєҐґ]").Count + Regex.Matches(b, @"[\p{P}\s]").Count) / Regex.Matches(x, @"[а-щА-ЩЬьЮюЯяЇїІіЄєҐґ]").Count != Convert.ToInt32(false)) || (val % (Regex.Matches(b, @"[а-щА-ЩЬьЮюЯяЇїІіЄєҐґ]").Count + Regex.Matches(b, @"[\p{P}\s]").Count) == Convert.ToInt32(true == false)) ? $"Так, {b}! {Environment.NewLine}Шкіряні приречені!{Environment.NewLine}" : $"Ні, на жаль, не {(DateTime.Now.Year == val ? x : DateTime.Now.Year > val ? "впорався" : $"з{x}")}...{Environment.NewLine}" : $"Звісно може, ви навіть рік не змогли ввести вірно!{Environment.NewLine}");
 }
